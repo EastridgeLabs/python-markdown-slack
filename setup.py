c77a474066995fe7851c568c93e53180c8d96863
Rye Gongora <rsgmon@gmail.com>
2023-04-11 13:10:45 -0700

    Update setup.py
    Update reference to markdown version and repo version.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='python-markdown-slack',
-    version='1.0.6',
+    version='1.0.7',
     py_modules=['python_markdown_slack', 'python_markdown_slack.list_handler'],
-    install_requires=['markdown==2.6.6'],
+    install_requires=['markdown==3.4.3'],
     license='MIT'
 )
