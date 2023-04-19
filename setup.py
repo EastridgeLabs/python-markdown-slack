29180e904f25a439802cefd258f8eaa55fbc7b35
Rye Gongora <rsgmon@gmail.com>
2023-04-19 11:39:28 -0700

    EAS-9250: Revert Changes
    Revert markdown change

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='python-markdown-slack',
     version='1.0.7',
     py_modules=['python_markdown_slack', 'python_markdown_slack.list_handler'],
-    install_requires=['markdown==2.6.11'],
+    install_requires=['markdown==3.4.3'],
     license='MIT'
 )
