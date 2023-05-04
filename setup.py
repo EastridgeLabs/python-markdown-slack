4faaf04676751abbb05efdd05dcd60fb5a57472b
Rye Gongora <rsgmon@gmail.com>
2023-05-04 15:13:24 -0700

    EAS-9250: 3.11 update
    Change markdown to highest version before 3.4 to avoid error.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='python-markdown-slack',
     version='1.0.7',
     py_modules=['python_markdown_slack', 'python_markdown_slack.list_handler'],
-    install_requires=['markdown==3.4.3'],
+    install_requires=['markdown==3.3.7'],
     license='MIT'
 )
